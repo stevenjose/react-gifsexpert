cb4359c4b17c22fbe0f06c25be7b4e9d612bcd60
Jose lopez <lopezajoseg@gmail.com>
2020-06-26 15:41:16 -0400

    apy-key

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,8 +1,8 @@
 export const getGifs = async (category) => {
   //console.log(category);
   //A8xMXqzieIHmtO3BjGLAtf1daSSDAv8K
-  //const url = `http://api.giphy.com/v1/gifs/search?api_key=biZpTaJhj5YsOHN7Arrs9Wz9GNWM959q&q=${encodeURI(
-    const url = `http://api.giphy.com/v1/gifs/search?api_key=A8xMXqzieIHmtO3BjGLAtf1daSSDAv8K&q=${encodeURI(  
+  const url = `http://api.giphy.com/v1/gifs/search?api_key=biZpTaJhj5YsOHN7Arrs9Wz9GNWM959q&q=${encodeURI(
+    //const url = `http://api.giphy.com/v1/gifs/search?api_key=A8xMXqzieIHmtO3BjGLAtf1daSSDAv8K&q=${encodeURI(  
     category
   )}&limit=12`;
   const resp = await fetch(url);
